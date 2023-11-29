26018f053df680a8f949fefd7ffeed3f9c6e9801
hibaasim <hiba.asim10@gmail.com>
2023-11-28 18:38:24 -0800

    tasks 3 -> 7

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for n in range(00, 99):
+    print('{:02d}'.format(n), end=', ')
+print('{}'.format(99))
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+def islower(c):
+    for i in range(97, 123):
+        if ord(c) == i:
+            return True
+        else:
+            return False
