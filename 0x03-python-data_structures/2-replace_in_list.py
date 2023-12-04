9e3adfbe5367d686842ee8a48d7365d449bbbff2
hibaasim <hiba.asim10@gamil.com>
2023-12-05 00:54:50 +0200

    0 -> 6

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pyhton3
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        my_list.pop(idx)
+        my_list.insert(idx, element)
+    return (my_list)
