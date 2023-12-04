4c052e6856bfc61b30d31334aa86a8fc987872df
hibaasim <hiba.asim10@gmail.com>
2023-12-04 15:25:19 -0800

    fixes

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):
         new = my_list.copy()
         new[idx] = element
-    return (new)
-
+        return (new)
+    else:
+        return (my_list)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for n in range(len(matrix)):
         for i in range(len(matrix[n])):
-                print('{:d}'.format(matrix[n][i]), end='')
-                if i != (len(matrix[n]) - 1):
-                    print(' ', end="")
+            print('{:d}'.format(matrix[n][i]), end='')
+            if i != (len(matrix[n]) - 1):
+                print(' ', end="")
         print('')
