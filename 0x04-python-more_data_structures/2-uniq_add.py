c59722c323f9f99e22dd0b9f0f6792902ffbdc80
hibaasim <hiba.asim10@gamil.com>
2023-12-07 02:57:29 +0200

    fixes

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,4 +3,4 @@ def uniq_add(my_list=[]):
     ans = 0
     for i in set(my_list):
         ans  = ans + i
-    return (ans) 
+    return (ans)
--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
     com_set = {x for x in (set_1 & set_2)}
     return (com_set)
