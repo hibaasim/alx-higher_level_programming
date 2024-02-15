ab65e28972196bb8df1437527a034d0e8e7e5b3b
hibaasim <hiba.asim10@gmail.com>
2024-02-15 10:06:32 -0800

    6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
-        (id INT NOT NULL AUTO_INCREMENT,
-        PRIMARY KEY(id),
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	PRIMARY KEY(id),
+        id INT NOT NULL AUTO_INCREMENT,
         name NOT NULL VARCHAR(256));
