1ba15352b895ef6d7679d9533b1dd1ecdb126928
hibaasim <hiba.asim10@gmail.com>
2024-02-15 17:59:22 -0800

    fix 6->10

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---Creates a table second_table in the database and adds multiple rows
+-- Creates a table second_table in the database and adds multiple rows
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table(id, name, score) VALUES (2, 'Alex', 3);
