031b80ab775bda83599fe5b2b554318d146509d4
hibaasim <139067889+hibaasim@users.noreply.github.com>
2024-02-27 11:03:43 +0200

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++
@@ -1,8 +0,0 @@
--- lists all Comedy shows.
-SELECT ts.title
-FROM tv_shows AS ts INNER JOIN tv_show_genres AS sg
-ON ts.id = sg.show_id
-INNER JOIN tv_genres AS tg
-ON sg.genre_id = tg.id
-WHERE tg.name = 'Comedy'
-ORDER by ts.title;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++
@@ -1,7 +0,0 @@
--- Lists all shows, and all genres linked to that show
-SELECT ts.title AS title, tg.name AS name
-FROM tv_shows AS ts LEFT JOIN tv_show_genres AS sg
-ON ts.id = sg.show_id
-LEFT JOIN tv_genres AS tg
-ON tg.id = sg.genre_id
-ORDER BY ts.title, tg.name;
