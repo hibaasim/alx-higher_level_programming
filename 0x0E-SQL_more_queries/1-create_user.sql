6e0890a298721f64d6184f7c27bae63c847e6193
Hiba <hiba.asim10@gmail.com>
2024-03-13 19:12:22 -0700

    not fixed

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,9 @@
--- Creates the MYSQL user_0d_1.
-DROP USER 'user_0d_1'@'localhost'
+-- Creates the MYSQL user user_0d_1 with all privileges.
 CREATE USER
-	IF NOT EXISTS 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-	ON *.*
-	TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
