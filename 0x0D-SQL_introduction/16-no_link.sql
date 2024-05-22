16beb5f8c10612c3fad49987e8090fa14b51f162
hibaasim <hiba.asim10@gmail.com>
2024-05-21 19:57:33 -0700

    last tasks

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,3 @@
---lists all records of the table second_table
-SELECT score, name
-FROM second_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;
+-- display score and name from table second_table
+-- if name is null will not be display
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
