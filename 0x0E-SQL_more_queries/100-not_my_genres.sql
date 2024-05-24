79a0ac50f67e3cae22dff3b6dec08782c128466b
hibaasim <hiba.asim10@gmail.com>
2024-05-24 00:57:03 -0700

    fixes

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -9,7 +9,7 @@ WHERE g.name NOT IN
       (SELECT name
 	FROM tv_genres AS g
 	INNER JOIN tv_show_genres AS sg
-	ON g.id = s.genre_id
+	ON g.id = sg.genre_id
 	INNER JOIN tv_shows AS s
 	ON sg.show_id = s.id
 	WHERE s.title = "Dexter")
