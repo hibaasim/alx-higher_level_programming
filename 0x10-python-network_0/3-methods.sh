db161980b2e85a66a8a6c1850b459d7ac45d0974
hibaasim <hiba.asim10@gmail.com>
2024-07-07 09:33:33 -0700

    task-3-fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept.
-curl -Is "$1" | grep Allow | cut -d " " -f 2-4
+curl -Is "$1" | grep Allow | cut -d " " -f 2-
